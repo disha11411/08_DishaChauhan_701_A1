56e8d500b9ba0ada244c76540233a769012da9dc
disha11411 <dishachauhan.mscit22@vnsgu.ac.in>
2025-07-21 13:27:50 +0530

    Add files via upload

---
+++ que2/public/script.js
@@ -0,0 +1,5 @@
+// This script runs when the page loads
+window.onload = function() {
+  console.log("Static site script.js loaded successfully!");
+  alert("Welcome to the static server example!");
+};
